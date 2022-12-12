2da9a6eca3dd147a84b7c780704e00d1b96299f5
jcurtis <jcurtis@cvent.com>
2022-12-11 22:24:51 -0400

    day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,52 +1,54 @@
-use itertools::Itertools;
-
 #[aoc(day1, part1)]
-fn part_1(input: &str) -> u32 {
-    input
-        .trim()
-        .split("\n\n")
-        .map(|elf| {
-            elf.lines()
-                .map(|item| item.trim().parse::<u32>().unwrap())
-                .sum()
-        })
-        .max()
-        .unwrap()
+fn part_1(input: &str) -> i32 {
+    input.lines().map(solve).sum()
+}
+
+fn solve(input: &str) -> i32 {
+    solve_int(input.parse::<i32>().unwrap())
+}
+
+fn solve_int(input: i32) -> i32 {
+    (input as i32 / 3) - 2
+}
+
+fn solve_rec(fuel: i32) -> i32 {
+    let fuel = solve_int(fuel);
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + solve_rec(fuel)
+    }
 }
 
 #[aoc(day1, part2)]
-fn part_2(input: &str) -> u32 {
+fn part_2(input: &str) -> i32 {
     input
-        .trim()
-        .split("\n\n")
-        .map(|elf| {
-            elf.lines()
-                .map(|item| item.trim().parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .sorted()
-        .rev()
-        .take(3)
+        .lines()
+        .map(|line| solve_rec(line.parse().unwrap()))
         .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
     use super::*;
+
+    const INPUT_1: &str = "12";
+    const INPUT_2: &str = "14";
+    const INPUT_3: &str = "1969";
+    const INPUT_4: &str = "100756";
 
     #[test]
     fn test_part_1() {
-        let input = fs::read_to_string("test_input/day01.txt").expect("Error reading file");
-
-        assert_eq!(part_1(&input), 24000);
+        assert_eq!(solve(INPUT_1), 2);
+        assert_eq!(solve(INPUT_2), 2);
+        assert_eq!(solve(INPUT_3), 654);
+        assert_eq!(solve(INPUT_4), 33583);
     }
 
     #[test]
     fn test_part_2() {
-        let input = fs::read_to_string("test_input/day01.txt").expect("Error reading file");
-
-        assert_eq!(part_2(&input), 45000);
+        assert_eq!(solve_rec(14), 2);
+        assert_eq!(solve_rec(1969), 966);
+        assert_eq!(solve_rec(100756), 50346);
     }
 }
