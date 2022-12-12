07c0efe8a767e0fa029f0c4d20d71bd0b9969daa
jcurtis <jcurtis@cvent.com>
2022-12-11 21:49:59 -0400

    fork 2022 for 2019

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-pub mod day01;
-pub mod day02;
-pub mod day03;
-pub mod day04;
-pub mod day05;
-pub mod day06;
-pub mod day07;
-pub mod day08;
-pub mod day09;
-pub mod day10;
-pub mod day11;
+// pub mod day01;
+// pub mod day02;
+// pub mod day03;
+// pub mod day04;
+// pub mod day05;
+// pub mod day06;
+// pub mod day07;
+// pub mod day08;
+// pub mod day09;
+// pub mod day10;
+// pub mod day11;
 
 extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
 
-aoc_lib! { year = 2022 }
+aoc_lib! { year = 2019 }
